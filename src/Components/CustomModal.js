4135c1ef7cda461ec573d99e1842e02a38b08f13
ShakilJoy31 <joy31a@gmail.com>
2023-12-27 06:50:41 +0600

    Done

--- src/Components/CustomModal.js
+++ src/Components/CustomModal.js
@@ -4,7 +4,7 @@ import HomeComponentCss from '../../style/ComponentStyle.module.css';
 
 const CustomModal = ({ closeModal }) => {
     const handleCheckAdmin = (inputValue) => {
-        if (inputValue === 'iamadmin') {
+        if (inputValue === 'password') {
             closeModal();
             localStorage.setItem('authenticAdmin', JSON.stringify('authenticAdmin'));
         }
@@ -19,9 +19,9 @@ const CustomModal = ({ closeModal }) => {
                 }} className="modal-box">
                     <h3 className="flex justify-center text-white items-center gap-x-2"><span><TbAlertOctagonFilled size={30} color={'black'}></TbAlertOctagonFilled></span> <span>Hey, Attention please!</span></h3>
 
-                    <h3 className="flex justify-center text-white my-2">If you are authenticated, you should obviously know the secret password. Type it below to procceed.</h3>
+                    <h3 className="flex justify-center text-white my-2">If you are an admin, Type your password here.</h3>
 
-                    <textarea onChange={(e) => handleCheckAdmin(e.target.value)} style={{ background: 'purple' }} type="text" className="w-full pt-2 input focus:outline-none input-md " />
+                    <textarea onChange={(e) => handleCheckAdmin(e.target.value)} style={{ background: 'purple' }} type="text" placeholder='Type your password here.' className="w-full pt-2 input focus:outline-none input-md " />
 
                 </div>
             </div>
